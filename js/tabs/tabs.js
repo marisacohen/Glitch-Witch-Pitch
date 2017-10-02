34d7c32bc59d309ddc74455f55aa2888a465067b
Alexandra Rezk <31907778+AlexandraRezk@users.noreply.github.com>
2017-10-02 19:31:26 -0400

    Add files via upload

--- js/tabs/tabs.js
+++ js/tabs/tabs.js
@@ -1,20 +1,19 @@
-$('.character-nav').each(function(){                   // Find lists of tabs
+$('.character-nav').each(function(){               // Find lists of tabs
   var $this = $(this);                            // Store this list
-  var $tab = $this.find('li.active');             // Get the active list item
-  var $link = $tab.find('a');                     // Get link from active tab
+  var $tab = $this.find('li.active');           // Get the active list item
+  var $link = $tab.find('a');                   // Get link from active tab
   var $panel = $($link.attr('href'));             // Get active panel
 
   $this.on('click', '.tab-control', function(e) { // When click on a tab
     e.preventDefault();                           // Prevent link behavior
     var $link = $(this),                          // Store the current link
-        id = this.hash;                           // Get href of clicked tab 
+        id = this.hash;                          // Get href of clicked tab 
 
-    if (id && !$link.is('.active')) {             // If not currently active
-      $panel.removeClass('active');               // Make panel inactive
+    if (id && !$link.is('.active')) {           // If not currently active
       $tab.removeClass('active');                 // Make tab inactive
-
-      $panel = $(id).addClass('active');          // Make new panel active
-      $tab = $link.parent().addClass('active');   // Make new tab active 
+      $panel.removeClass('active');               // Make panel inactive
+        $tab = $link.parent().toggleClass('active');   // Toggles between tabs with active class
+      $panel = $(id).toggleClass('active');        // Toggles between panels with active class
     }
   });
 });
