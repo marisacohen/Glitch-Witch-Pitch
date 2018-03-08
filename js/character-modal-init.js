1399861b56c25f320881d9c45922761198f376e3
Alexandra Rezk <31907778+AlexandraRezk@users.noreply.github.com>
2018-03-07 20:35:30 -0500

    Updated the Character Page
    Updated the Modal Window to resize with the screen and the size of the image.

--- js/character-modal-init.js
+++ js/character-modal-init.js
@@ -1,6 +1,6 @@
 (function(){
     var $content = $('#character-relation-closeup').detach();
     $('#character-relation').on('click', function(){
-        modal.open({content: $content, width: 1200, height: 780});
+        modal.open({content: $content, width: 680, height: 700});
     });
 }());
